10403b727e7d7eb0cea93bb5a54ee1497fb1c416
MrEBONG <thierryngabe@icloud.com>
2024-02-28 15:01:24 +0100

    Done

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
-// api integration
+// var age =12
+// var name ='john'
+
+// var sentence = name +' ' + 'is ' + age; //john is 12
+
+// var phrase = `${name} is ${age}`; // john is 12
+
+//api  integration
 var container = document.querySelector('.card-container');
-function getProducts() {
-  fetch("https://fakestoreapi.com/products")
+
+ function getProducts() { 
+    fetch("https://fakestoreapi.com/products")
     .then((result) => result.json())
     .then((products) => {
       container.innerHTML = products.map(product=>{
-        return ` <div class="card">
-            <div class="image-content">
-                <img src="${product.image}" width="100%" height="100%">
+        return `        <div class="card">
+        <h4 class="men"><i>men's clothing</i></h4>           
+        <div class="img-contain">                
+            <img src="${product.image}" width="100%" height="100%" alt="Bag image">
+        </div>
+        <div class="desc">
+            <h3 class="title">${product.title.slice(0,20)}... </h3>
+            <div class="stars">
+                <i class="fas fa-star"${product.rate}></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
             </div>
-            <div class="desc">
-                <h3 class="title">${product.title.slice(0,20)}...</h3>
-                 <h4>${product.price} XAF</h4>
-                 <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                 </div>
-                 <p class="description">${product.description.slice(0,90)}</p>
-            </div>
-            <button class="addToCart">Add to basket</button>
-        </div>`
-      })
-    });
+            <p class="description">${product.description.slice(0,70)}</p>
+        </div>
+        <div class="md-5">XAF</div>
+        <button class="addToCard">Add To Basket</button>
+    </div>`
+      }).join(" ")
+
+    })
+ }
+
+ 
+ getProducts();
+
+
+
+
+
+
+
+var category = document.querySelector('.categories')
+function getCategory() { 
+  fetch("https://fakestoreapi.com/products/categories")
+  .then((result) => result.json())
+  .then((products) => {
+    category.innerHTML += products.map(category=>{
+     return ` <p class="line">${category}</p>`
+    }).join(" ")
+  });
 }
-getProducts();
+
+ getCategory();
