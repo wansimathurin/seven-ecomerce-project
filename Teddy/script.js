daff532ec46b764490fc0891de77eb0abac20b1c
Ngangman-Teddy <teddy.webdev@2005.com>
2024-02-29 19:05:57 +0100

    entering last stage

--- Teddy/script.js
+++ Teddy/script.js
@@ -2,27 +2,29 @@ const form = document.querySelector(".form");
 const p_name = document.getElementById("p_name");
 const price = document.getElementById("price");
 const qty = document.getElementById("qtt");
-// const desc = document.getElementById("message")
+const desc = document.querySelector("#message")
+const imagescr = document.querySelector("img").src;
 
 form.addEventListener('submit', e => {
-	e.preventDefault();
-	
-	checkInputs();
+    e.preventDefault();
+
+    checkInputs();
 });
 
 function checkInputs() {
     const nameValue = p_name.value.trim();
     const priceValue = price.value.trim();
-    // const descValue = desc.Value;
     const qtyValue = qty.value.trim();
+    const descValue = desc.value.trim();
 
-    if(nameValue === ''){
+
+    if (nameValue === '') {
         setErrorFor(p_name, 'Product name cannot be blank');
     } else {
         setSuccessFor(p_name);
     }
 
-    if(priceValue === ''){
+    if (priceValue === '') {
         setErrorFor(price, 'Add price');
     } else if (isNaN(priceValue)) {
         setErrorFor(price, 'use a number')
@@ -31,21 +33,30 @@ function checkInputs() {
         setSuccessFor(price);
     }
 
-    if(qtyValue === ''){
+    if (qtyValue === '') {
         setErrorFor(qty, 'Add Quntity');
-    } else if(isNaN(qtyValue)) {
+    } else if (isNaN(qtyValue)) {
         setErrorFor(qty, 'use a number')
     }
-     else {
+    else {
         setSuccessFor(qty);
     }
 
-    // if(descValue === ''){
-    //     setErrorFor(qty, 'Add Quntity');
-    //     console.log('error1')
-    // } else {
-    //     setSuccessFor(desc);
-    // }
+    if (descValue === '') {
+        setErrorFor(desc, 'Product description cant be empty');
+    } else {
+        setSuccessFor(desc);
+    }
+
+    var data = {
+        'product_name': nameValue,
+        'product_price': priceValue,
+        'product_qty': qtyValue,
+        'product_desc': descValue,
+        'product_image': imagescr,
+    }
+
+    console.table(data)
 
 }
 
@@ -57,8 +68,34 @@ function setErrorFor(input, message) {
 }
 
 function setSuccessFor(input) {
-	const formControl = input.parentElement;
-	formControl.className = 'form_group success';
+    const formControl = input.parentElement;
+    formControl.className = 'form_group success';
 }
 
-console.log("teddy")
+// this code helps upload image from the computer
+
+var loadFile = function (event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+// using links
+
+document.querySelector(".button_preview").addEventListener('click', function preview() {
+
+    var img = new Image();
+    img.src = document.getElementById("image_link").value;
+    
+    // The URL isn't valid or the resource isn't a picture
+    img.onerror = function() { alert("Provided URL does not point to a valid picture.") };
+    
+    // Ok, we have correct picture; display it
+    img.onload = function() {
+        document.getElementById("output").src = img.src;
+    };
+
+})
+
+    
+
+// 
