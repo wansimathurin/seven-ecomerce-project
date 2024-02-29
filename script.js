65db5562a026f636b67045c3e75e10ef870ee2aa
Destiny653 <fokundem.com@gmail.com>
2024-02-29 16:37:01 +0100

    PopUp

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
-// api integration
-var container = document.querySelector('.card-container');
-function getProducts() {
-  fetch("https://fakestoreapi.com/products")
-    .then((result) => result.json())
-    .then((products) => {
-      container.innerHTML = products.map(product=>{
-        return ` <div class="card">
-            <div class="image-content">
-                <img src="${product.image}" width="100%" height="100%">
-            </div>
-            <div class="desc">
-                <h3 class="title">${product.title.slice(0,20)}...</h3>
-                 <h4>${product.price} XAF</h4>
-                 <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                 </div>
-                 <p class="description">${product.description.slice(0,90)}</p>
-            </div>
-            <button class="addToCart">Add to basket</button>
-        </div>`
-      }).join(" ")
-    });
+
+//PopUp
+//Stating Variables
+
+var popUpIm = document.querySelector(".scrollPop");
+var show = document.querySelector('.show');
+var headTitle = document.querySelector('.title');
+var price = document.querySelector('.price');
+var description = document.querySelector('.description');
+var category = document.querySelector('.levels')
+
+//looping through cards 
+
+function activate(e) {
+    const card = document.querySelectorAll('.card');
+    e.target.matches(".next") && popUpIm.append(card[0]);
+    console.log(card[0])
+    e.target.matches(".prev") && popUpIm.prepend(card[card.length - 1]);
+
 }
-getProducts();
+
+// calling fuction to action with help of EventListener
+
+document.addEventListener('click', activate, false);
+
+// stating fuction to call the object of the API
+function popMenu() {
+    fetch('https://fakestoreapi.com/products')
+        .then((result) => result.json())
+        .then((products) => {
+            popUpIm.innerHTML = products.map((product) => {
+                return `
+            <div class="card">
+            <img src="${product.image}" alt="">
+            </div> `
+            }).join("")
+
+            const card = document.querySelectorAll(".card")
+
+
+            card.forEach((item, i) => {
+                item.addEventListener('click', () => {
+
+                    category.innerHTML = products[i].category;
+                    show.src = products[i].image;
+                    headTitle.innerText = products[i].title;
+                    description.innerHTML = products[i].description;
+                    price.innerHTML = products[i].price + "XAF";
+                });
+                console.log(products[i]);
+            })
+
+
+        });
+}
+popMenu();
